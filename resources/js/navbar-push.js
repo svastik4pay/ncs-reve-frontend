ba969c066d7a3a455e93028c41ea4d5e42bcf885
svastik4pay <123751634+svastik4pay@users.noreply.github.com>
2023-02-13 15:12:01 -0500

    jan-13 update

---
+++ resources/js/navbar-push.js
@@ -0,0 +1,12 @@
+// $(function() {
+
+//     $('.offcanvas').on('click', function(){
+//         $('.wrapper').toggleClass('open');   
+//      });
+// });
+
+$('.play').click(function() {
+    $('.all-content').css({
+        'transform': 'translateX(80%)'
+});
+});
