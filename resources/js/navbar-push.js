c1b4e4e5a338828cb3e1baffcf449a861cb28eba
svastik4pay <123751634+svastik4pay@users.noreply.github.com>
2023-02-14 13:35:56 -0500

    Add files via upload

--- resources/js/navbar-push.js
+++ resources/js/navbar-push.js
@@ -5,8 +5,17 @@
 //      });
 // });
 
-$('.play').click(function() {
-    $('.all-content').css({
-        'transform': 'translateX(80%)'
-});
-});
+function toggle(){
+var button = document.querySelector('#toggler-nav');
+
+if(button.clicked == true){
+document.querySelector('#toggler-nav').addEventListener('click', () => {
+document.querySelector('#content').classList.add('open');
+  });
+}
+else{
+    document.querySelector('#content').classList.add('close'); 
+}
+}
+
+toggle();
